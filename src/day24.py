e5e80431e5f5f2d0260753169621b4cc74076620
Wojciech Mostowski <wojciech.mostowski@gmail.com>
2021-12-24 18:19:33 +0100

    Day 24 cleaned up

--- src/day24.py
+++ src/day24.py
@@ -6,59 +6,31 @@ file.close()
 
 part2 = True
 
-listing = [""] * 18
-
-def expand(d):
-	while len(d) < 10:
-		d = d + " "
-	return d
+consts1 = []
+consts2 = []
+consts3 = []
 
 for d in data:
 	if d == 'inp w':
 		cnt = 0
-	listing[cnt] = listing[cnt] + expand(d)
+	if cnt == 4:
+		consts3.append(int(d[6:]))
+	elif cnt == 5:
+		consts1.append(int(d[6:]))
+	elif cnt == 15:
+		consts2.append(int(d[6:]))
 	cnt += 1
 
-#for l in listing:
-#	print(l)
+rel_pos = {}
+max_pos = []
+stack = []
 
-"""
-mul x 0
-add x z
-mod x 26
-div z 1		# const3
-add x 11	# const1
-eql x w
-eql x 0
-mul y 0
-add y 25
-mul y x
-add y 1
-mul z y
-mul y 0
-add y w
-add y 6		# const2
-mul y x
-add z y
-"""
-
-def step(w, z, const1, const2, const3):
-	x = 0 if w == z % 26 + const1 else 1
-	z = z // const3
-	# const3 == 1 -> leave untouched (keep building)
-	# const3 == 26 -> revert to previous
-	z = z*(25*x + 1) + x*(w + const2)
-	return z
-
-consts1 = [11, 13, 15, -8, 13, 15, -11, -4, -15, 14, 14, -1, -8, -14]
-consts2 = [6,  14, 14, 10,  9, 12,   8, 13,  12,  6,  9, 15,  4,  10]
-consts3 = [1,  1,   1, 26,  1,  1,  26, 26,  26,  1,  1, 26,  26, 26]
-
-# These are input specific and could be calculated from the
-# consts3 array above
-
-rel_pos = {3 : 2, 6 : 5,  7 : 4, 8 : 1, 11 : 10, 12 : 9, 13 : 0}
-max_pos = [0, 1, 2, 4, 5, 9, 10]
+for p,c3 in enumerate(consts3):
+	if c3 == 1:
+		max_pos.append(p)
+		stack.append(p)
+	else:
+		rel_pos[p] = stack.pop()
 
 digits = [x for x in range(1,10)]
 if not part2:
